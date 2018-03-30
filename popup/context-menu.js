2ad4fffe2dddf5ac7062b2535a2e179195c3b08b
Greg Fussell <charlesfussell@ufl.edu>
2018-03-30 17:26:53 -0400

    added message passing

--- popup/context-menu.js
+++ popup/context-menu.js
@@ -4,7 +4,7 @@ function listenForClicks()
 	document.addEventListener("click", (e) => {
 		// Log something happened
 		console.log(`Memex7: You did somthing!`);
-
+		notifyBackgroundPage(e);
 		// Just log the error to the console
 		function reportError(error) {
 			console.error(`There was an error: ${error}`);
@@ -19,6 +19,21 @@ function reportExecuteScriptError(error)
 	document.querySelector("#popup-content").classList.add("hidden");
 	document.querySelector("#error-content").classList.remove("hidden");
 	console.error(`Failed to execute Memex7 content script: ${error.message}`);
+}
+function handleResponse(message) {
+  console.log(`Message from the background script:  ${message.response}`);
+}
+
+function handleError(error) {
+  console.log(`Error: ${error}`);
+}
+
+
+function notifyBackgroundPage(e) {
+  var sending = browser.runtime.sendMessage({
+    greeting: "Greeting from the content script"
+  });
+  sending.then(handleResponse, handleError);  
 }
 
 console.log(`Memex7: Was loaded!`);
